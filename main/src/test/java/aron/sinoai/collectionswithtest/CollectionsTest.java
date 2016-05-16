49c0877c2c5b27f3afe335db6edf76951aab153f
asinoai <aron.sinoai@linkonomics.com>
2016-05-16 21:15:49 +0300

    Initial revision

---
+++ main/src/test/java/aron/sinoai/collectionswithtest/CollectionsTest.java
@@ -0,0 +1,14 @@
+package aron.sinoai.collectionswithtest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CollectionsTest {
+
+    @Test
+    public void basicTest() {
+        final int value = 0;
+
+        Assert.assertEquals(1, value);
+    }
+}
